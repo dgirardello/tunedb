cf30206a256efb940b812519a767299949e62153
damian <damian_girardello@mcafee.com>
2017-09-12 11:07:00 -0300

    added links

--- lib/Tune.py
+++ lib/Tune.py
@@ -25,9 +25,11 @@ class Tune(object):
     def change_meter_note(self, beat_str, base_note, conversion_note=16):
         pass
 
-    def transpose(self, beat_str, base_key, conversion_key='Cmay'):
+    def transpose_to_C(self, beat_str, base_key):
+        # https://method-behind-the-music.com/theory/scalesandkeys/#transposition
         pass
 
     def get_note_duration(self, beat_str):
+        # http://abcnotation.com/wiki/abc:standard:v2.2#note_lengths
         pass
 
