4622b4705280ecab0a68089867fa9c0ba0c3352c
damian <damian_girardello@mcafee.com>
2017-09-12 17:23:32 -0300

    added requirements.txt

--- lib/Tune.py
+++ lib/Tune.py
@@ -1,3 +1,7 @@
+import os
+import re
+import json
+import music21
 
 class Tune(object):
     name = ''
@@ -11,12 +15,23 @@ class Tune(object):
     tune_norm = ''
 
     def __init__(self, source):
+        if os.path.isfile(source):
+            with open(source, 'r') as f:
+                file_name = os.path.basename(source)
+                if file_name.endswith('abc'):
+                    self.parse_abc_tune(f)
+                elif file_name.endswith('json'):
+                    self.parse_json_tune(f)
+        elif isinstance(source, str):
+            if is_abc_tune(source):
+                self.parse_abc_tune(source)
+            elif is_json_tune(source):
+                self.parse_json_tune(source)
+
+    def parse_abc_tune(self, abc_text):
         pass
 
-    def parse_abc(self, abc_text):
-        pass
-
-    def parse_json(self, abc_text):
+    def parse_json_tune(self, abc_text):
         pass
 
     def normalize_tune(self):
@@ -32,4 +47,28 @@ class Tune(object):
     def get_note_duration(self, beat_str):
         # http://abcnotation.com/wiki/abc:standard:v2.2#note_lengths
         pass
+
+
+def is_abc_tune(tune_str):
+    if not re.match('T\:[\s]]?.*', tune_str): return False
+    if not re.match('R\:[\s]]?\w*', tune_str): return False
+    if not re.match('M\:[\s]]?\d\\\d', tune_str): return False
+    if not re.match('L\:[\s]]?\d\\\d', tune_str): return False
+    if not re.match('K\:[\s]]?\w*', tune_str): return False
+    if not re.match('\|.*\|', tune_str): return False
+    return True
+
+
+def is_json_tune(tune_str):
+    try:
+        loaded_tune = json.loads(tune_str)
+    except ValueError:
+        return False
+    if not 'name' in loaded_tune: return False
+    if not 'type' in loaded_tune: return False
+    if not 'metric' in loaded_tune: return False
+    if not 'unit' in loaded_tune: return False
+    if not 'key' in loaded_tune: return False
+    if not 'tune_abc' in loaded_tune: return False
+    return True
 
