20562def8af5c1132f00f6988b75d6a86594d542
Damián <nanodag@gmail.com>
2019-09-24 23:11:43 -0300

    commit db

---
+++ lib/db.py
@@ -0,0 +1,25 @@
+import os
+import sqlite3
+from lib import constants
+
+DB_FILE_PATH = os.path.abspath(constants.DB_FILE)
+
+
+def get_connection():
+    return sqlite3.connect(DB_FILE_PATH)
+
+
+def query_db_select(query):
+    conn   = get_connection()
+    cursor = conn.cursor()
+    cursor.execute(query)
+    return cursor.fetchall()
+
+
+def query_db_insert(table, columns, values):
+    conn   = get_connection()
+    cursor = conn.cursor()
+    query = "INSERT INTO {table}({columns}) VALUES({values})".format(table=table, columns=','.join(columns), values='?' * len(columns))
+    cursor.execute(query, values)
+    conn.commit()
+    return cursor.lastrowid
--- script/abc_to_DB.py
+++ script/abc_to_DB.py
@@ -3,13 +3,13 @@ from lib import db, constants
 
 ABC_PATH = '../db/abc'
 KEY_REGEX = "([{keys}]):(.+)".format(keys=''.join(constants.KEY_REFERENCE.keys()))
+DB_TABLE = 'library'
 
 
 abc_files = []
 for file in os.listdir(os.path.abspath(ABC_PATH)):
     if file.endswith(".abc"):
         abc_files.append(os.path.join(os.path.abspath(ABC_PATH),file))
-
 for file in abc_files:
     file_obj = open(file, 'r')
     file_lines = file_obj.readlines()
@@ -17,16 +17,16 @@ for file in abc_files:
     values_used = []
     tune_text = ""
     for line in file_lines:
-        if re.search(KEY_REGEX) is not None:
-            key, value = re.findall(KEY_REGEX)
-            keys_used.append(constants.KEY_REFERENCE[key])
-            values_used.append(value)
-        else:
-            tune_text += line + "\n"
-    keys_used.append(constants.KEY_REFERENCE("F"))
+        if line.find("|") >= 0:
+            tune_text += str(line)
+        elif re.match(KEY_REGEX, line) is not None:
+            key, value = re.findall(KEY_REGEX, line.strip())[0]
+            keys_used.append(constants.KEY_REFERENCE[key.strip()])
+            values_used.append(value.strip())
+    keys_used.append(constants.KEY_REFERENCE["F"])
     values_used.append(file)
     keys_used.append("tune_text")
     values_used.append(tune_text)
 
-    print(keys_used)
-    print(values_used)
+    id = db.query_db_insert(table=DB_TABLE, columns=keys_used, values=values_used)
+    print(id)
